41caf5ca7a26d393586ac1c06ceeae625607f38a
Liubou <alekseevna0201@gmail.com>
2018-11-09 13:27:38 +0300

    Add win music, add code for timer, add styles

---
+++ js/js.js
@@ -0,0 +1,203 @@
+'use strict';
+
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+var cvsWidth = 1024; // ширина
+var cvsHeight = 520; //высота
+var plane = new Image();
+var bg = new Image(); // Создание объекта
+var obstacleUp = new Image(); // Создание объекта
+var obstacleBottom = new Image(); // Создание объекта
+var house = new Image(); // Создание объекта
+var score = 0;
+var time = -1;
+var musicBox = document.querySelector('#mus');
+
+var xPos = 10; //позиция самолетика
+var yPos = 150;
+var grav = 1.5; // скорость притяжения к земле
+var gap = 200; /// доступное расстояние для обхода препятствий
+
+// обработка аудио
+var radio = new Audio();
+radio.src = "audio/music.mp3";
+var hitAudio = new Audio();
+hitAudio.src = "audio/zvuk-udar.mp3";
+var scoreAudio = new Audio();
+scoreAudio.src = "audio/score.mp3";
+var winAudio = new Audio();
+winAudio.src = "audio/win.mp3";
+
+var divForPlay = document.querySelector('#forPlay');
+var divForPlayAgain = document.querySelector('#lose');
+var mainDiv = document.querySelector('#main-background');
+var winDiv = document.querySelector('#bg-win');
+var sBox = document.getElementById('time');
+var pauseBox = document.getElementById('pause');
+var rulesBox = document.getElementById('rules');
+
+house.src = "img/house.png";
+plane.src = "img/plane1.png"; // Указание нужного изображения
+bg.src = "img/bg1.jpg"; // Аналогично
+obstacleUp.src = "img/rock.png"; // Аналогично
+obstacleBottom.src = "img/tree.png"; // Аналогично
+
+//самолет вверх при клике
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+  yPos -= 20;
+  xPos += 3;
+}
+
+//создание препятствий
+var obstacle = [];
+obstacle[0] = {
+  x: cvs.width / 2,
+  y: 0
+};
+
+////////////////////////////////ОТРИСОВКА!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function draw() {
+  yPos += grav;
+  ctx.drawImage(bg, 0, 0);
+
+  for (var i = 0; i < obstacle.length; i++) {
+    ctx.drawImage(obstacleUp, obstacle[i].x, obstacle[i].y);
+    ctx.drawImage(obstacleBottom, obstacle[i].x, obstacle[i].y + obstacleUp.height + gap);
+    obstacle[i].x--;
+
+    if (obstacle[i].x === 450) {
+      obstacle.push({
+        x: cvs.width,
+        y: Math.floor(Math.random() * obstacleUp.height) - obstacleUp.height
+      });
+    }
+
+    if (xPos + plane.width >= obstacle[i].x
+      && xPos <= obstacle[i].x + obstacleUp.width
+      && (yPos <= obstacle[i].y + obstacleUp.height
+        || yPos + plane.height >= obstacle[i].y + obstacleUp.height + gap)
+      || yPos + plane.height >= cvs.height) {
+
+      document.getElementById('time').innerHTML = time;
+      hitAudio.play();
+      scoreAudio.pause();
+      ctx.clearRect(0, 0, cvs.width, cvs.height);
+      playAgain();
+    }
+
+    if (obstacle[i].x === plane.x - 40) {
+      score++;
+      scoreAudio.play();
+    }
+  }
+  ctx.drawImage(plane, xPos, yPos, 100, 70);
+
+  ctx.fillStyle = '#048';
+  ctx.font = '50px Verdana';
+  ctx.shadowColor = 'rgb(50,50,50)';
+  ctx.shadowOffsetX = 2;
+  ctx.shadowOffsetY = 3;
+  ctx.shadowBlur = 3;
+  ctx.fillText('Счет: ' + score, cvs.width / 2, 50);
+
+  if (score === 2) {
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+    hitAudio.pause();
+    ctx.fillStyle = '#000';
+    ctx.font = '40px Verdana';
+    ctx.fillText('Уровень успешно пройден! Поздравляем!', 25, 100);
+    winAudio.play();
+    winDiv.style.display = 'block';
+  }
+  requestAnimationFrame(draw);
+}
+
+musicBox.onclick = function () {
+  /* проверяем если музыка не на паузе воспроизводим*/
+  if (radio.paused === true) {
+    radio.play();
+  } else {
+    radio.pause();
+  }
+};
+
+function startGame() {
+  //////////////////////////////////   TIMER SECOND !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  (function () {
+    var times = 30;
+
+    timer(times);
+
+    function timer(times) {
+      var tm = setInterval(function () {
+        times--;
+        if (times === 0) {
+          clearInterval(tm);
+          playAgain();
+        } else if (times === -1) {
+          times = 30;
+          times--;
+        }
+        var sec = (times < 10) ? '0' + times : times;
+
+        // выводим значение таймера на экран
+        showTimer(sec);
+      }, 1000);
+    }
+
+    function showTimer(s) {
+      sBox.innerHTML = s;
+    }
+  })();
+  obstacleBottom.onload = draw();
+  divForPlay.style.display = 'none';
+  mainDiv.style.display = 'none';
+  pauseBox.style.cursor = 'pointer';
+}
+
+function playAgain() {
+  sBox.innerHTML = 'TIMER';
+  hitAudio.pause();
+  divForPlayAgain.style.display = 'block';
+  mainDiv.style.display = 'block';
+  divForPlayAgain.style.display = 'block';
+}
+
+function reload() {
+  location.reload();
+}
+
+//  рисуем спрайт РЕШИТЬ ПРОБЛЕМУ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+// var width = 100,
+//   height = 67,
+//   frames = 3,
+//
+//   currentFrame = 0;
+//
+// var image = new Image();
+// image.src = 'img/sprite.png';
+//
+// var drawSprite = function () {
+//   ctx.clearRect(0, 0, width, height);
+//   ctx.drawImage(image, 300, height * currentFrame, width, height, 0, 0, width, height);
+//
+//   if (currentFrame === frames) {
+//     currentFrame = 0;
+//   } else {
+//     currentFrame++;
+//   }
+// };
+// setInterval(drawSprite, 100);
+
+pauseBox.onclick = function () {
+  confirm('Игра остановлена. Для продолжения нажмите OK');
+};
+
+rulesBox.onclick = function () {
+  confirm('Цель игры - обойти 10 препятствий за 30 секунд.\n' +
+    'Для управления самолетом нажимайте любую клавишу\n' +
+    '\n Для продолжения нажмите OK');
+};
+
---
+++ js/spa.js
@@ -0,0 +1,75 @@
+'use strict';
+
+window.onhashchange = SwitchToStateFromURLHash;
+
+var SPAStateH={};
+
+var PhotosH=
+  {
+    1 : { image:"img/Hilu3.jpg", comment:"собака Шарик" },
+    2 : { image:"img/Retriever3.jpg", comment:"собака Барбос" }
+  };
+
+function SwitchToStateFromURLHash () {
+  var URLHash=window.location.hash;
+
+  var StateJSON=decodeURIComponent(URLHash.substr(1));
+
+  if ( StateJSON!=="" )
+
+    SPAStateH=JSON.parse(StateJSON); // если JSON непустой, читаем из него
+                                     // состояние и отображаем
+  else
+    SPAStateH={pagename:'Main'}; // иначе показываем главную страницу
+
+  console.log('Новое состояние приложения:');
+  console.log(SPAStateH);
+
+  // обновляем вариабельную часть страницы под текущее состояние
+  var PageHTML="";
+
+  switch ( SPAStateH.pagename ) {
+    case 'Main':
+      PageHTML+="<h3>Главная страница</h3>";
+      PageHTML+="<p>Щёлкайте по кнопкам!</p>";
+      break;
+    case 'Photo':
+      var PhotoH=PhotosH[SPAStateH.photoid];
+      PageHTML+="<h3>Фото</h3>";
+      PageHTML+="<img src='"+PhotoH.image+"'>";
+      PageHTML+="<p><i>"+PhotoH.comment+"</i></p>";
+      break;
+    case 'About':
+      PageHTML+="<h3>О нас</h3>";
+      PageHTML+="<p>Мы круты!</p>";
+      break;
+  }
+  document.getElementById('IPage').innerHTML=PageHTML;
+}
+
+function SwitchToState(NewStateH) {
+  location.hash=encodeURIComponent(JSON.stringify(NewStateH));
+}
+
+function SwitchToMainPage() {
+  SwitchToState( { pagename:'Main' } );
+}
+
+function SwitchToPhotoPage(PhotoId) {
+  SwitchToState( { pagename:'Photo', photoid:PhotoId } );
+}
+
+function SwitchToAboutPage() {
+  SwitchToState( { pagename:'About' } );
+}
+
+SwitchToStateFromURLHash();
+
+var PhotosStr="";
+for ( var PhotoId in PhotosH )
+{
+  var PhotoH=PhotosH[PhotoId];
+  PhotosStr+='<input type=button value="'+PhotoH.comment
+    +'" onclick="SwitchToPhotoPage('+PhotoId+')"> ';
+}
+document.getElementById('IPhotosButtons').innerHTML = PhotosStr;
